9633a7e00e33205a6feb8d0f32b2328dde8ba89d
oleg <oleg.lutsenko@adyen.com>
2018-07-13 12:44:23 +0200

    Update

--- lib/Order.php
+++ lib/Order.php
@@ -72,9 +72,9 @@ class Order
         return $this->channel;
     }
 
-    public $sdkVersion = "1.3.0";
+    public $sdkVersion = "1.4.0";
 
-    public function getSDKVersion() 
+    public function getSDKVersion()
     {
         return $this->sdkVersion;
     }
